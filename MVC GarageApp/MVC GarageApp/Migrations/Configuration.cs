a689719ecf0a440b2a038db99ab94d224e606256
Taleb Mosyri <taleb_n1@hotmail.com>
2017-12-10 23:48:21 +0100

    Update sorting

--- MVC GarageApp/MVC GarageApp/Migrations/Configuration.cs
+++ MVC GarageApp/MVC GarageApp/Migrations/Configuration.cs
@@ -20,53 +20,159 @@ namespace MVC_GarageApp.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-            context.ParkeraVehicles.AddOrUpdate(x => x.RegistrationNumber,
-                new ParkedVehicle()
-                {
-                    Type = Models.Type.Car,
-                    RegistrationNumber = "ABC123",
-                    Color = "Red",
-                    Brand = "Ferrari",
-                    Model = "2017",
-                    NumberOfWheels = 4,
-                },
-                new ParkedVehicle()
-                {
-                    Type = Models.Type.Motorcycle,
-                    RegistrationNumber = "MC639",
-                    Color = "Black-orange",
-                    Brand = "BMW",
-                    Model = "2010",
-                    NumberOfWheels = 2,
-                },
-                new ParkedVehicle()
-                {
-                    Type = Models.Type.Boat,
-                    RegistrationNumber = "MC639",
-                    Color = "White",
-                    Brand = "Sad-eye",
-                    Model = "2000",
-                    NumberOfWheels = 0,
-                },
-                new ParkedVehicle()
-                {
-                    Type = Models.Type.Airplane,
-                    RegistrationNumber = "CAR007",
-                    Color = "Silver",
-                    Brand = "F-16",
-                    Model = "2014",
-                    NumberOfWheels = 6,
-                });
+            //context.ParkeraVehicles.AddOrUpdate(x => x.RegistrationNumber,
+            //    new ParkedVehicle()
+            //    {
+            //        Type = Models.Type.Car,
+            //        RegistrationNumber = "ABC123",
+            //        Color = "Red",
+            //        Brand = "Ferrari",
+            //        Model = "2017",
+            //        NumberOfWheels = 4,
+            //    },
+            //    new ParkedVehicle()
+            //    {
+            //        Type = Models.Type.Motorcycle,
+            //        RegistrationNumber = "MC640",
+            //        Color = "Black-orange",
+            //        Brand = "BMW",
+            //        Model = "2010",
+            //        NumberOfWheels = 2,
+            //    },
+            //    new ParkedVehicle()
+            //    {
+            //        Type = Models.Type.Boat,
+            //        RegistrationNumber = "MC639",
+            //        Color = "White",
+            //        Brand = "Sad-eye",
+            //        Model = "2000",
+            //        NumberOfWheels = 0,
+            //    },
+            //    new ParkedVehicle()
+            //    {
+            //        Type = Models.Type.Airplane,
+            //        RegistrationNumber = "CAR007",
+            //        Color = "Silver",
+            //        Brand = "F-16",
+            //        Model = "2014",
+            //        NumberOfWheels = 6,
+            //    });
 
-            ////extend the database to 1000
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    context.ParkeraVehicles.AddOrUpdate(r => r.Type.ToString(),
+            //extend the database to 100
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
 
-            //         new ParkedVehicle { Type =(Models.Type)i, RegistrationNumber=i.ToString(),
-            //         Color = i.ToString(), Brand =i.ToString(), Model =i.ToString(),
-            //          });
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Car,
+                         RegistrationNumber = "CAR00"+i.ToString(),
+                         Color = "White",
+                         Brand = "Mercedes",
+                         Model = "2010",
+                         NumberOfWheels = 4,
+                     });                
+              }
+
+            for (int i = 0; i <10 ; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Car,
+                         RegistrationNumber = "JPL0" + i.ToString()+"6",
+                         Color = "Red",
+                         Brand = "Ferrari",
+                         Model = "2016",
+                         NumberOfWheels = 4,
+                     });
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Boat,
+                         RegistrationNumber = "BOA" + i.ToString()+"82",
+                         Color = "Blue",
+                         Brand = "Yamaha",
+                         Model = "2010",
+                         NumberOfWheels = 0,
+                     });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Boat,
+                         RegistrationNumber = "PLK" + i.ToString() + "99",
+                         Color = "Yellow/White",
+                         Brand = "Kawasaki",
+                         Model = "2016",
+                         NumberOfWheels = 0,
+                     });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Airplane,
+                         RegistrationNumber = "ÄPÅ" + i.ToString()+"47",
+                         Color = "White",
+                         Brand = "Airbus",
+                         Model = "2009",
+                         NumberOfWheels = 6,
+                     });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Airplane,
+                         RegistrationNumber = "PÖÄ" + i.ToString() + "78",
+                         Color = "Green",
+                         Brand = "Boeing",
+                         Model = "2013",
+                         NumberOfWheels = 8,
+                     });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Motorcycle,
+                         RegistrationNumber = "MCP00" + i.ToString(),
+                         Color = "Black",
+                         Brand = "Harley",
+                         Model = "2015",
+                         NumberOfWheels = 2,
+                     });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                context.ParkeraVehicles.AddOrUpdate(r => r.RegistrationNumber,
+
+                     new ParkedVehicle
+                     {
+                         Type = Models.Type.Motorcycle,
+                         RegistrationNumber = "JOI" + i.ToString()+"65",
+                         Color = "Silver",
+                         Brand = "Chopper",
+                         Model = "2014",
+                         NumberOfWheels = 2,
+                     });
             }
         }
     }
-
+}
