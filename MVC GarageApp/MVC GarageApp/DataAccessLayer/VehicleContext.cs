746ef68192622764c92215786a36feeceb667cf3
Taleb01 <taleb_n1@hotmail.com>
2017-12-11 14:33:35 +0100

    create a new branch

--- MVC GarageApp/MVC GarageApp/DataAccessLayer/VehicleContext.cs
+++ MVC GarageApp/MVC GarageApp/DataAccessLayer/VehicleContext.cs
@@ -9,7 +9,7 @@ namespace MVC_GarageApp.DataAccessLayer
     public class VehicleContext : DbContext
     {
         public VehicleContext() : base ("MVC-Garage") {}
-
+                                                                              
         public DbSet<Models.ParkedVehicle> Vehicles { get; set; }
     }
 }
